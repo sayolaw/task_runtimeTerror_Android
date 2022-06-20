517076c202fe7e0bdca825e05c8dfdaf0ed13dd8
Olaoluwa Lawal <shayolaw@gmail.com>
2022-06-20 16:52:41 -0400

    add subtask adapter

--- app/src/main/java/com/example/task_runtimeterror_android/Sub.java
+++ app/src/main/java/com/example/task_runtimeterror_android/Sub.java
@@ -1,4 +1,24 @@
 package com.example.task_runtimeterror_android;
 
 public class Sub {
+    private int id,taskid,status;
+    private String name;
+
+    public Sub(int id,int status, String name) {
+        this.id = id;
+        this.status = status;
+        this.name = name;
+
+    }
+
+    public int getId() {
+        return id;
+    }
+    public int getStatus() {
+        return status;
+    }
+
+    public String getName() {
+        return name;
+    }
 }

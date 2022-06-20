c7a02f0f3b0a42c1cca70e6972f2173a7f164bab
Olaoluwa Lawal <shayolaw@gmail.com>
2022-06-20 17:04:59 -0400

    corrects errors in view subtask module

--- app/src/main/java/com/example/task_runtimeterror_android/viewSubs.java
+++ app/src/main/java/com/example/task_runtimeterror_android/viewSubs.java
@@ -3,14 +3,24 @@ package com.example.task_runtimeterror_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class viewSubs extends AppCompatActivity {
     int id;
     String task;
+    SQLiteDatabase sqLiteDatabase;
+    List<Sub> subList;
+    ListView lv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +28,12 @@ public class viewSubs extends AppCompatActivity {
         Bundle bundle = getIntent().getExtras();
         id = bundle.getInt("id");
         task = bundle.getString("name");
+        lv = findViewById(R.id.subList);
+
+        subList = new ArrayList<>();
+        sqLiteDatabase = openOrCreateDatabase("subtasks",MODE_PRIVATE,null);
+
+        loadSubs();
     }
     public void addSub(View view){
         Intent intent = new Intent(this, addSubs.class);
@@ -28,6 +44,31 @@ public class viewSubs extends AppCompatActivity {
         intent.putExtras(bundle);
         startActivity(intent);
 
+    }
+    private void loadSubs() {
+        String sql = "SELECT * FROM subtasks";
+        Cursor cursor = sqLiteDatabase.rawQuery(sql, null);
+
+
+
+        if (cursor.moveToFirst()) {
+            Log.d("here","we are here");
+            do {
+                // create an employee instance
+
+                subList.add(new Sub(
+                        cursor.getInt(0),
+                        cursor.getInt(1),
+                        cursor.getString(2)
+
+                ));
+
+            } while (cursor.moveToNext());
+            cursor.close();
+        }
+
+        subAdapter subAdapt = new subAdapter(this,R.layout.sub_layout,subList,sqLiteDatabase);
+        lv.setAdapter(subAdapt);
     }
 
 }
